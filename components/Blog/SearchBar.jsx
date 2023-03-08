b54ea9840d0767d16c854fb945d4e84a47bf6341
MrRevillod <lrevillod2022@alu.uct.cl>
2023-03-08 16:16:20 -0300

    searchbarIcon

--- components/Blog/SearchBar.jsx
+++ components/Blog/SearchBar.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import Image from 'next/image'
 
 export const SearchBar = ({ searchTerm, setSearchTerm }) => {
     return (
-        <div className="md:w-1/2 w-full flex items-center justify-end md:ml-5">
+        <div className="md:w-1/2 w-full flex items-center justify-end md:ml-5 relative">
             <input
                 type="text"
                 placeholder="Buscar por nombre"
@@ -10,6 +11,15 @@ export const SearchBar = ({ searchTerm, setSearchTerm }) => {
                 onChange={(e) => setSearchTerm(e.target.value)}
                 className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:border-transparent text-zinc-800"
             />
+
+            <Image
+                src='http://pillan.inf.uct.cl/~lrevillod/Blog/assets/icons/search-icon.png'
+                width={20}
+                height={20}
+                alt="Search Icon"
+                style={{ position: 'absolute', right: '10px', top: '11px' }}
+            />
+
         </div>
     )
 }
