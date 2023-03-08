fe62f65477f38cdc64809bd46e86915d4920c2db
MrRevillod <lrevillod2022@alu.uct.cl>
2023-03-07 21:08:48 -0300

    fixed images

--- components/Blog/CodeBox.jsx
+++ components/Blog/CodeBox.jsx
@@ -2,15 +2,8 @@ import React from 'react'
 import Image from 'next/image'
 import { useState } from 'react'
 
-const useCodeBoxImageClass = () => {
-    const isMobile = typeof window !== 'undefined' && window.innerWidth < 1048
-
-    return isMobile ? '' : 'md:w-6 md:h-6'
-}
-
-export const CodeBox = ({ text }) => {
+export const CodeBox = ({ text, showImg }) => {
     const [copied, setCopied] = useState(false)
-    const imageClass = useCodeBoxImageClass()
 
     const handleCopy = () => {
         navigator.clipboard.writeText(text)
@@ -18,17 +11,22 @@ export const CodeBox = ({ text }) => {
         setTimeout(() => setCopied(false), 2000) // Reset copied status after 2 seconds
     }
 
+    showImg = showImg === '' || showImg === undefined || showImg === null ? true : showImg = false
+
     return (
         <div className="relative rounded-md bg-zinc-800 p-4 my-4">
             <div className="absolute top-3.5 right-3 cursor-pointer" onClick={handleCopy}>
-                <div className="max-w-full">
-                    <Image
-                        src="http://pillan.inf.uct.cl/~lrevillod/Blog/assets/icons/copiar_img.png"
-                        alt="Copy to clipboard"
-                        width={24}
-                        height={24}
-                    />
-                </div>
+
+                {showImg && (
+                    <div className="max-w-full">
+                        <Image
+                            src="http://pillan.inf.uct.cl/~lrevillod/Blog/assets/icons/copiar_img.png"
+                            alt="Copy to clipboard"
+                            width={24}
+                            height={24}
+                        />
+                    </div>
+                )}
             </div>
             <pre className="text-gray-100 font-mono text-sm max-w-full overflow-x-auto">$ {text}</pre>
         </div>
