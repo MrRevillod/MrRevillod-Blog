bc48f8c7e604a74b8b749ea88a3134dd0dd11f5e
MrRevillod <lrevillod2022@alu.uct.cl>
2023-03-08 14:31:01 -0300

    update

--- components/Blog/SearchBar.jsx
+++ components/Blog/SearchBar.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 
 export const SearchBar = ({ searchTerm, setSearchTerm }) => {
     return (
-        <div className="md:w-1/2 w-full flex items-center justify-end">
+        <div className="md:w-1/2 w-full flex items-center justify-end md:ml-5">
             <input
                 type="text"
                 placeholder="Buscar por nombre"
                 value={searchTerm}
                 onChange={(e) => setSearchTerm(e.target.value)}
-                className="w-full md:w-11/12 px-4 py-2 border rounded-lg focus:outline-none focus:border-transparent text-zinc-800"
+                className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:border-transparent text-zinc-800"
             />
         </div>
     )
