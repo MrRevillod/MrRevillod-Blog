2a1d5ddc8ca58f8bb972aace21713a68b56c7cb8
MrRevillod <lrevillod2022@alu.uct.cl>
2023-03-09 01:19:46 -0300

    favicon

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -5,12 +5,13 @@ export default function MyDocument() {
 		<Html lang="en">
 			<Head>
 
-				<link rel="icon" href="/favicon.ico" />
+				<link rel="icon" href="/public/Terminal-favicon.png" />
 
 				{/* <a target="_blank" href="https://icons8.com/icon/1JOOtg5aq3Ek/chevron">chevron</a> icon by <a target="_blank" href="https://icons8.com">Icons8</a> */}
 				{/* <a target="_blank" href="https://icons8.com/icon/DydnsatR799b/chevron">chevron</a> icon by <a target="_blank" href="https://icons8.com">Icons8</a> */}
 				{/* <a target="_blank" href="https://icons8.com/icon/nxZZ7qE7Ad2v/download-file">Download File</a> icon by <a target="_blank" href="https://icons8.com">Icons8</a> */}
 				{/* <a target="_blank" href="https://icons8.com/icon/7695/b%C3%BAsqueda">Búsqueda</a> icon by <a target="_blank" href="https://icons8.com">Icons8</a> */}
+				{/* <a target="_blank" href="https://icons8.com/icon/A4dv9LFftxza/consola">Consola</a> icon by <a target="_blank" href="https://icons8.com">Icons8</a> */}
 
 			</Head>
 			<body>
