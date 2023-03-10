080999ca4429ee448a6078089812c0519ddc1691
MrRevillod <lrevillod2022@alu.uct.cl>
2023-03-10 17:43:22 -0300

    update

--- components/Portfolio/SkillCard.jsx
+++ components/Portfolio/SkillCard.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import Image from 'next/image'
 
 export const SkillCard = ({ title, icon, description }) => {
 	return (
 		<div className="bg-zinc-700 hover:bg-zinc-600 hover:-translate-y-2 transform transition rounded-md p-5">
 			<div className="text-gray-300 w-6 h-6 mb-3">
-				<img src={icon} alt="dev-skill" />
+				<Image src={icon} alt="dev-skill" width={25} height={25} />
 			</div>
 			<h1 className="font-semibold text-lg text-gray-200 mb-1">
 				{title}
